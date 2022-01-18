dd7fd8fb1f342ad8b9aff07dd9f5018600934197
Sumna Islam <sumanislam1416@gmail.com>
2022-01-18 23:06:56 +0600

    add views and complete welcome page views

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 
 const userRouter = require('./routes/users.router');
 
@@ -6,11 +7,16 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+// views
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+// routes
 app.use('/users', userRouter);
 
 app.get('/', (req, res) => {
-  res.send('Welcome');
-})
+  res.render('welcome');
+});
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}...`);
