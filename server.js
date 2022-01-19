a866babaff583b1f08ed8dd902dbc5eedddf59c6
Sumna Islam <sumanislam1416@gmail.com>
2022-01-19 07:46:48 +0600

    connect mongodb with project and create user schema and model

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 
 const userRouter = require('./routes/users.router');
+
+require('dotenv').config();
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const config = {
+  MONGO_URI: process.env.MONGO_URI,
+}
+
 // views
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
+
+// connect mongodb
+mongoose.connect(config.MONGO_URI, () => {
+  console.log('mongodb is connected');
+})
 
 // routes
 app.use('/users', userRouter);
